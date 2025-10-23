d128bc8bf6e2f80fc911884ccec1a85065d0ab1b
Eli-Myers <emyers8623@sdsu.edu>
2025-10-23 10:33:44 -0700

    Implemented push, pop, parent/child helper functions, and started upheap/downheap

--- heap.h
+++ heap.h
@@ -11,25 +11,51 @@ using namespace std;
 struct MinHeap {
     int data[64];
     int size;
+    //Helper methods for parent and left/right children
+    int parentIdx(int i){return (i-1)/2;}
+    int leftChildIdx(int i){return (i*2)+1;}
+    int rightChildIdx(int i){return (i*2)+2;}
 
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        if(size >= 64) {
+            cout << "Can't push, full heap!" << endl;
+        } else {
+            data[size] = idx;
+            size++;
+            //upheap(size, weightArr);
+        }
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if(size == 0){
+            cout << "Can't pop, empty heap!" << endl;
+            return 0;
+        }
+        int smallest = data[0];
+        data[0] = data[size - 1];
+        size--;
+        downheap(size, weightArr);
+        return smallest;
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        if(size == 0) return;
+
+        upheap(leftChildIdx(pos), weightArr);
+        upheap(rightChildIdx(pos), weightArr);
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        if(size == 0) return;
+
+
     }
 };
 
